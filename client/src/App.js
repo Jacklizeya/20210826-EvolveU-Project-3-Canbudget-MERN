e70a060d74e17bab1799f070da538b805244045c
Iryna <iryna.tar18@gmail.com>
2021-06-21 15:53:22 -0600

    created navbar, routes for landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
+import { BrowserRouter  as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Promotions from './components/pages/Promotions';
+import Advisors from './components/pages/Advisors';
+import Budget from './components/pages/Budget';
+import Asset from './components/pages/Asset';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>CanBudget</h1>
-	<p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/promotions' exact component={Promotions} />
+        <Route path='/advisors' exact component={Advisors} />
+        <Route path='/budget' exact component={Budget} />
+        <Route path='/asset' exact component={Asset} />
+        <Route path='/sign-up' exact component={SignUp} />
+      </Switch>
+    </Router>
   );
 }
 
