7aef10f80c095ce32f374d74ea892ec1a29ab36e
Jacklizeya <jacklizeya@gmail.com>
2021-06-25 13:04:35 -0600

    CRUD-Update functionality on 2 pages

--- client/src/components/pages/Asset.js
+++ client/src/components/pages/Asset.js
@@ -7,8 +7,8 @@ function Asset() {
     const [type, setType] = useState("")
     const [value, setValue] = useState(0)
     const [changeMonthToMonth, setChangeMonthToMonth] = useState(0)
-    const [status, setStatus] = useState(0)
- 
+    const [addstatus, setAddStatus] = useState(0)
+    const [deleteStatus, setDeleteStatus] = useState(0)
 
     useEffect(() => {
         async function getUsers() {
@@ -17,8 +17,9 @@ function Asset() {
             setUsers(data)
         }
         getUsers()
-        setStatus(0)
-    }, [status])
+        setAddStatus(0)
+        setDeleteStatus(0)
+    }, [addstatus, deleteStatus])
 
 
     async function addNewBalanceSheet(event, id) {
@@ -31,9 +32,34 @@ function Asset() {
             setType("")
             setValue(0)
             setChangeMonthToMonth(0)
-            setStatus(data.ok)
+            setAddStatus(data.ok)
         }
     }
+
+    async function deleteBalanceSheet(event, id) {
+        let nameOfItemToRemove = event.target.value
+        console.log(nameOfItemToRemove)
+        let {data} = await axios.put(`/api/user/${id}/deletebalancesheet/`, {nameOfItemToRemove}, {headers : {"Content-Type": "application/json"}})
+        if (data.ok) {
+            setDeleteStatus(data.ok)
+        }
+    }
+
+    function editItem(event) {
+    // continue tomorrow
+    console.log("edit", users, event.target.id)
+    let index = event.target.id
+    // Right now I am using users[0], eventually it will be just one user, so need to fix this later
+    let dataToEdit = users[0].balanceSheet[index]
+    console.log(dataToEdit)
+    setName(dataToEdit.name)
+    setType(dataToEdit.type)
+    setValue(dataToEdit.value)
+    setChangeMonthToMonth(dataToEdit.changeMonthToMonth)
+
+    }
+
+
 // right now only hook up one, wait until login is done 
  return (
     <>
@@ -62,13 +88,31 @@ function Asset() {
                         <tbody>
 
                         {user.balanceSheet.map(
-                            singleBalanceSheet => 
+                            (singleBalanceSheet, index) => 
 
                             <tr key={singleBalanceSheet.name}>
                                 <td> {singleBalanceSheet.name} </td>
                                 <td> {singleBalanceSheet.type} </td>
                                 <td> {singleBalanceSheet.value} </td>
-                                <td> {singleBalanceSheet.changeMonthToMonth} </td>                   
+                                <td> {singleBalanceSheet.changeMonthToMonth} </td>    
+                                <td> 
+                                    <a href="#form">
+                                        <button id={index} onClick={editItem} > 
+                                            Edit 
+                                        </button>
+                                    </a>
+                                    
+
+                                    
+                                </td> 
+                                <td>
+                                    <button onClick={(event)=> deleteBalanceSheet(event, user._id)} value={singleBalanceSheet.name}>
+                                    delete 
+                                    </button> 
+                                </td>     
+                                
+
+
                             </tr>
                             )} 
 
@@ -76,8 +120,8 @@ function Asset() {
                     </table>    
                 
                 <br/> 
-                <div className="form">
-                    Add new item 
+                <div className="form" id="form">
+                    Add new item/Edit existing item 
                     <form onSubmit={(event) => addNewBalanceSheet(event, user._id)}>
                         <label> Name of the item </label>
                         <input type="text" value={name} onChange={(event)=>{setName(event.target.value)}}/> <br/>
