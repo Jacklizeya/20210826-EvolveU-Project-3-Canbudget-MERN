67cb88187f061f81734fedd9a2fccfe68e3af807
Vladimir Zhukov <Zhukovtnv@gmail.com>
2021-06-27 23:32:53 -0600

    Login code

--- server/models/user.js
+++ server/models/user.js
@@ -33,7 +33,28 @@ async function createUser(newUser) {
     return userModel.create(newUser);
 }
 
+async function findById(id){
+    try {
+        return userModel.findById(id);      
+    } catch (error) {
+        console.error("error=", error);
+    }
+    return null;
+}
+
+async function checkLogin(email, password){
+    try {
+        return userModel.findOne({email, password}, 'userType');      
+    } catch (error) {
+        console.error("error=", error);
+    }
+    return null;
+}
+
+
 module.exports = {
     createUser,
-    getUserList
+    getUserList,
+    findById, 
+    checkLogin
 }
