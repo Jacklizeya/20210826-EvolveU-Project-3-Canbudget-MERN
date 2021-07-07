6ea47a4c100d81cf6601e158207932ede63ab9d6
Brady Walker-Brock <bwalkerbr@gmail.com>
2021-07-06 19:27:01 -0600

    google search bar appearing

--- client/src/components/ApexCharts/GoogleLocations/SearchableMap.js
+++ client/src/components/ApexCharts/GoogleLocations/SearchableMap.js
@@ -1,8 +1,7 @@
-import React, { useEffect, useState, useMemo } from "react"
+import React, {onLoad, onPlacesChanged, useState} from "react"
 import {
   GoogleMap,
-  Marker,
-  InfoWindow,
+  StandaloneSearchBox,
   useLoadScript
 } from "@react-google-maps/api"
 import mapStyles from "./mapStyles"
@@ -13,101 +12,64 @@ const mapContainerStyle = {
   height: "685px",
   border: "4px solid #05386B",
   borderRadius: "20px",
-  minWidth: "575px"
+  minWidth: "775px"
 }
-const center = {
-  lat: 51.01,
-  lng: -114.1
-}
+
 const options = {
   styles: mapStyles,
   disableDefaultUI: true,
   zoomControl: true
 }
 
-export default function SearchableMap({
-  isFormDisplayed,
-  formCoordinates,
-  setFormCoordinates
-}) {
+export default function SearchableMap() {
+
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: 'AIzaSyCPw4VRivOAyVV9WZGpwal6eRZJSIZh1KY',
     libraries
   })
 
-  // This is passed through the first Marker array
-  const loadingMessage = [{name: 'Loading...', address: "This won't take long!", "coordinates":{"lat":"0","lng":"0"}}]
-  const [gardenList, setGardenList] = useState(loadingMessage)
-  
-  useEffect(() => {
-    const getAllGardens = async () => {
-      let fetchUrl = "/api/garden/get"
-      let response = await fetch(fetchUrl)
-      let resObject = await response.json()
-      let listResult = resObject.gardenList
+  const [mapCenter, setMapCenter] = useState( {
+    lat: 51.01,
+    lng: -114.1
+  })
 
-      setGardenList(listResult)
-    }
-    getAllGardens()
-  }, [])
+  if (loadError) return "Error loading map"
+  if (!isLoaded) return "Loading map"
 
-  // Prevent re-rendering of data
-  const data = useMemo(() => gardenList, [gardenList])
-
-  const onMapClick = React.useCallback(
-    (event) => {
-      setFormCoordinates({
-        lat: event.latLng.lat(),
-        lng: event.latLng.lng()
-      })
-    },
-    [setFormCoordinates]
-  )
-
-  useEffect(() => {
-    if (isFormDisplayed) {
-      setFormCoordinates({
-        lat: 0,
-        lng: 0
-      })
-    }
-    // eslint-disable-next-line
-  }, [isFormDisplayed])
-
-  const [selected, setSelected] = React.useState(null)
-
-  if (loadError) return "Error loading maps"
-  if (!isLoaded) return "Loading Maps"
   return (
-    <div>
+    <div style={{display:'flex',justifyContent:'center',padding:'20px'}}>
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
         zoom={10.5}
-        center={center}
+        center={mapCenter}
         options={options}
       >
-        {data.map(function (marker, index) {
-          return (
-            <Marker
-              key={marker.name}
-              position={{lat: parseFloat(marker.coordinates.lat), lng: parseFloat(marker.coordinates.lng)}}
-              onMouseOver={() => {
-                setSelected(marker)
-              }}
-            />
-          )
-        })}
-
-        {selected ? (
-          <InfoWindow
-            position={{lat: parseFloat(selected.coordinates.lat), lng: parseFloat(selected.coordinates.lng)}}
-            onCloseClick={() => {
-              setSelected(null)
-            }}
-          >
-            <div style={{ fontWeight: "bold" }}>{selected.name}</div>
-          </InfoWindow>
-        ) : null}
+        <StandaloneSearchBox
+            onLoad={onLoad}
+            onPlacesChanged={
+                onPlacesChanged
+            }
+        >
+      <input
+        type="text"
+        placeholder="Customized your placeholder"
+        style={{
+          boxSizing: `border-box`,
+          border: `1px solid transparent`,
+          width: `240px`,
+          height: `32px`,
+          padding: `0 12px`,
+          borderRadius: `3px`,
+          boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+          fontSize: `14px`,
+          outline: `none`,
+          textOverflow: `ellipses`,
+          position: "absolute",
+          left: "50%",
+          marginLeft: "-120px"
+        }}
+      />
+    </StandaloneSearchBox>
       </GoogleMap>
     </div>
   )
