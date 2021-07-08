8d536d97a2d43cd83e301eec63af046da2cd2475
Brady Walker-Brock <bwalkerbr@gmail.com>
2021-07-08 10:09:46 -0600

    nearbySearchRequest functional

--- client/src/components/ApexCharts/GoogleLocations/NearbySearchRequest.js
+++ client/src/components/ApexCharts/GoogleLocations/NearbySearchRequest.js
@@ -1,18 +1,35 @@
 const axios = require('axios');
 
+// Search parameters
+let googleApiKey = 'AIzaSyCPw4VRivOAyVV9WZGpwal6eRZJSIZh1KY'
+let searchLat = 51.042642417966455
+let searchLng = -114.06990000957333
+let searchRadius = 10000 // Measured in m
+let searchType = 'bank'
+
+
 let url = ('https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
-  +'key=AIzaSyCPw4VRivOAyVV9WZGpwal6eRZJSIZh1KY'
-  +'&location=51.042642417966455, -114.06990000957333'
-  +'&radius=10000' // Measured in m
-  +'&type=bank')
+  +'key='+googleApiKey
+  +'&location='+searchLat+','+searchLng
+  +'&radius='+searchRadius
+  +'&type='+searchType)
 
-  async function makeGetRequest() {
+async function nearbySearchRequest() {
 
-    let res = await axios.get(url);
-  
-    let data = res.data;
-    console.log(data);
+  let res = await axios.get(url)
+
+  let data = res.data
+
+  let searchData = []
+  for (const index in data.results) {
+    let result = data.results[index]
+    if (result.business_status === 'OPERATIONAL') {
+      let trimmedResult = {name: result.name, geometry: result.geometry, rating: result.rating, ratingCount: result.user_ratings_total, address: result.vicinity}
+      searchData.push(trimmedResult)
+    }
   }
+  return searchData
+}
   
-  makeGetRequest();
+  nearbySearchRequest()
 
