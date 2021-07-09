d5b4420de1289552cc2111824503ae85af4c327d
Vladimir Zhukov <Zhukovtnv@gmail.com>
2021-07-09 13:26:56 -0600

    Login components added

---
+++ client/src/components/Stocks/Stocks.js
@@ -0,0 +1,11 @@
+import SmartSearch from '../Stocks/SmartsSearch';
+const Stocks = () => {
+    return (
+        <>
+            <h3>Stocks</h3>
+            {SmartSearch()}
+        </>
+    );
+}
+
+export default Stocks;
