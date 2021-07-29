0c29816a70aae8a47d769aa5c4a672192581e651
Brady Walker-Brock <bwalkerbr@gmail.com>
2021-07-28 18:41:01 -0600

    onboard form styling

--- client/src/components/OnboardingWizard/components/BooleanRadiusButtons/BooleanRadioButtons.js
+++ client/src/components/OnboardingWizard/components/BooleanRadiusButtons/BooleanRadioButtons.js
@@ -8,7 +8,7 @@ export default function BooleanRadioButtons({sendDataToParent, questionPrompt}) 
 
   return (
     <form>
-      <p>{questionPrompt}</p>
+      {typeof questionPrompt === 'string' ? <p>{questionPrompt}</p> : questionPrompt}
       <input type="radio" id="yesOnboardRadio" name="booleanOnboardRadio" value='true' onClick={handleClick}></input>
       <label for="yesOnboardRadio">Yes</label><br></br>
       <input type="radio" id="noOnboardRadio" name="booleanOnboardRadio" value='false' onClick={handleClick}></input>
