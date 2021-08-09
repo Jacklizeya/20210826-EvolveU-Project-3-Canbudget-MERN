bb87e702c05dc6a39303b8d51552ddaf91cd6ae9
Brady Walker-Brock <bwalkerbr@gmail.com>
2021-08-08 21:41:08 -0600

    onboard transporation widget working

--- client/src/components/OnboardingWizard/widgets/Transportation/Transportation.js
+++ client/src/components/OnboardingWizard/widgets/Transportation/Transportation.js
@@ -8,92 +8,94 @@ export default function Transportation() {
     const [displayOnboardBody, setDisplayOnboardBody] = useState(false)
     const [userOwnsVehicle, setUserOwnsVehicle] = useState(null)
     const [userHasVehiclePayments, setUserHasVehiclePayments] = useState(null)
-    const [userVehicleDetailsEntered, setUserVehicleDetailsEntered] = useState(false)
+    const [vehiclePaymentDetails, setVehiclePaymentDetails] = useState(null)
     const [userHasInsurance, setUserHasInsurance] = useState(null)
-    const [userInsuranceDetailsEntered, setUserInsuranceDetailsEntered] = useState(false)
-    const [askTransportationCosts, setAskTransportationCosts] = useState(false)
+    const [insurancePaymentDetails, setInsurancePaymentDetails] = useState(null)
     const [userHasTransportationCosts, setUserHasTransportationCosts] = useState(null)
-
-
-    const handleDataFromUserOwnsVehicle = (data) => {
-      if (data === 'true') {
-        setUserOwnsVehicle(true)
-      } else if (data === 'false') {
-          setUserOwnsVehicle(false)
-          setAskTransportationCosts(true)
-      }
-    }
-
-    const handleDataFromUserHasVehiclePayments = (data) => {
-      if (data === 'true') {
-        setUserHasVehiclePayments(true)
-      } else if (data === 'false') {
-          setUserHasVehiclePayments(false)
-          setAskTransportationCosts(true)
-      }
-    }
-
-    const handleDataFromUserHasTransportationCosts = (data) => {
-      if (data === 'true') {
-        setUserHasTransportationCosts(true)
-      } else if (data === 'false') {
-          setUserHasTransportationCosts(false)
-      }
-    }
-
-    const handleVehiclePaymentDetailsSubmit = (event) => {
-        setUserVehicleDetailsEntered(true)
-    }
-
-    const handleDataFromUserHasInsurance = (data) => {
-      if (data === 'true') {
-        setUserHasInsurance(true)
-      } else if (data === 'false') {
-          setUserHasInsurance(false)
-          setAskTransportationCosts(true)
-      }
-    }
-
-    const handleInsuranceDetailsSubmit = (event) => {
-        setUserInsuranceDetailsEntered(true)
-        setAskTransportationCosts(true)
-    }
-
-    const handleHeaderClick = (event) => {
-        setDisplayOnboardBody(!displayOnboardBody)
-    }
+    const [transportationPaymentDetails, setTransportationPaymentDetails] = useState(null)
 
     return (
       <div className='onboard-container'>
-        <h2 className='onboard-heading' onClick={handleHeaderClick}>Transportation</h2>
+        <h2 className='onboard-heading' onClick={() => setDisplayOnboardBody(!displayOnboardBody)}>Transportation</h2>
         {displayOnboardBody ? <div>
           <p className='onboard-heading-body'>Everyone needs to get around somehow! Let's talk about your expenses related to transportation</p>
-          <BooleanRadioButtons sendDataToParent={handleDataFromUserOwnsVehicle} questionPrompt='Do you own your own vehicle?:'/>
-            {userOwnsVehicle === true ?
-              <div> 
-                <BooleanRadioButtons sendDataToParent={handleDataFromUserHasVehiclePayments} questionPrompt='Do you make payments on your vehicle?'/>
-                {userHasVehiclePayments === true ? 
-                  <RecurringPaymentForm
-                    sendDataToParent={handleVehiclePaymentDetailsSubmit}
-                    questionPrompt='Tell us about your vehicle payment:'
-                    enableAddRows={false}
-                  /> 
-                : null}
-                {userVehicleDetailsEntered ? 
-                  <div>
-                    <BooleanRadioButtons sendDataToParent={handleDataFromUserHasInsurance} questionPrompt='Do you have vehicle insurance?'/>
-                    {userHasInsurance === true ? 
-                      <RecurringPaymentForm
-                        sendDataToParent={handleInsuranceDetailsSubmit}
-                        questionPrompt='Tell us about your vehicle insurance'
-                        enableAddRows={false}
-                      />
-                    : null}
-                  </div>
-                : null}
-              </div>
-            : null}
-          {askTransportationCosts ? <BooleanRadioButtons sendDataToParent={handleDataFromUserHasTransportationCosts} questionPrompt='Do you have other costs related to transportation?'/> : null}
+          <BooleanRadioButtons
+            questionPrompt='Do you own your own vehicle?:' 
+            sendDataToParent={(data) => {
+              if (data === 'true') {
+                setUserOwnsVehicle(true)
+              } else if (data === 'false') {
+                setUserOwnsVehicle(false)
+              }
+            }}
+          />
+          {userOwnsVehicle === true ?
+            <div> 
+              <BooleanRadioButtons
+                questionPrompt='Do you make payments on your vehicle?' 
+                sendDataToParent={(data) => {
+                  if (data === 'true') {
+                    setUserHasVehiclePayments(true)
+                  } else if (data === 'false') {
+                    setUserHasVehiclePayments(false)
+                  }
+                }} 
+              />
+              {userHasVehiclePayments ? 
+                <RecurringPaymentForm
+                  questionPrompt='Tell us about your vehicle payment:'
+                  enableAddRows={false}
+                  sendDataToParent={(data) => {
+                    setVehiclePaymentDetails(data[0])
+                  }}
+                /> 
+              : null}
+              {vehiclePaymentDetails ? 
+                <div>
+                  <BooleanRadioButtons
+                    questionPrompt='Do you have vehicle insurance?'
+                    sendDataToParent={(data) => {
+                      if (data === 'true') {
+                        setUserHasInsurance(true)
+                      } else if (data === 'false') {
+                        setUserHasInsurance(false)
+                      }
+                    }}
+                  />
+                  {userHasInsurance === true ? 
+                    <RecurringPaymentForm
+                      questionPrompt='Tell us about your vehicle insurance'
+                      enableAddRows={false}
+                      sendDataToParent={(data) => {
+                        setInsurancePaymentDetails(data[0])
+                      }}
+                    />
+                  : null}
+                </div>
+              : null}
+            </div>
+          : null}
+          {(userOwnsVehicle === false || userHasVehiclePayments === false|| userHasInsurance === false || insurancePaymentDetails) ? 
+            <BooleanRadioButtons
+              questionPrompt='Do you have other costs related to transportation?' 
+              sendDataToParent={(data) => {
+                if (data === 'true') {
+                  setUserHasTransportationCosts(true)
+                } else if (data === 'false') {
+                  setUserHasTransportationCosts(false)
+                }
+              }}
+            />
+          : null}
+          {userHasTransportationCosts ?
+            <RecurringPaymentForm
+              questionPrompt='Tell use about your transportation costs (common expenses include transit passes or Uber costs)'
+              enableAddRows={true}
+              sendDataToParent={(data) => {
+                setTransportationPaymentDetails(data)
+              }}
+            />
+          : null}
         </div> : null}
       </div>
     )
