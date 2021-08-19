f0cea3612b8c3f0e1a35aa4bbe80cef006b8cf76
Brady Walker-Brock <bwalkerbr@gmail.com>
2021-08-19 11:50:26 -0600

    updated transaction bar graph

--- client/src/components/ApexCharts/TransactionChart.js
+++ client/src/components/ApexCharts/TransactionChart.js
@@ -4,13 +4,16 @@ import Chart from "react-apexcharts"
 // data is feed in here
 export default function TransactionChart({data}) {
 
-
+    console.log(data)
     const spendingRows = data.filter(row => row.values.amount < 0 && !row.values.category.includes("Transfer") && !row.values.category.includes("Payment"))
-
 
     let category = []
     spendingRows.forEach(row => {if (!category.includes(row.values.category)) {category.push(row.values.category)}})
 
+    for (let i in spendingRows) {
+      let splitCategories = spendingRows[i].values.category.split(', ')
+      spendingRows[i].values.category = splitCategories[splitCategories.length - 1]
+    }
 
     let sumByCategory = {}
     category.forEach(subcategory => sumByCategory[subcategory] = 0)
@@ -18,9 +21,20 @@ export default function TransactionChart({data}) {
         sumByCategory[row.values.category] += row.values.amount
     })
 
+    let keys = Object.keys(sumByCategory)
+
+    // let values = []
+    // for (let i in sumByCategory) {
+    //   values.push({
+    //     name: i,
+    //     data: [Number(sumByCategory[i] * -1).toFixed(2)],
+    //     type: 'bar'
+    //   })
+    // }
+
     let valuesRaw = Object.values(sumByCategory)
     let values = valuesRaw.map(value => -1 * Math.floor(value))
-    let keys = Object.keys(sumByCategory)
+    
  
     let options = {
         chart: {
@@ -38,9 +52,18 @@ export default function TransactionChart({data}) {
         },
         xaxis: {
           categories: keys,
-        }
+          title: {
+            text: 'Value ($)'
+          }
+        },
+        yaxis: {
+            title: {
+              text: 'Categories'
+            }
+        },
       }
     let series = [{
+        name: 'Monthly Total',
         data: values
       }] 
       
